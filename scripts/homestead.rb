08f642fddc734b7aba509212b8ff08be132cb1c4
Matt Lindesay <matt@lindesay.com>
2015-01-31 22:04:38 +0000

    Skip database creation if not set

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -64,15 +64,17 @@ class Homestead
     end
 
     # Configure All Of The Configured Databases
-    settings["databases"].each do |db|
-        config.vm.provision "shell" do |s|
-            s.path = "./scripts/create-mysql.sh"
-            s.args = [db]
-        end
-
-        config.vm.provision "shell" do |s|
-            s.path = "./scripts/create-postgres.sh"
-            s.args = [db]
+    if settings.has_key?("databases")
+        settings["databases"].each do |db|
+            config.vm.provision "shell" do |s|
+                s.path = "./scripts/create-mysql.sh"
+                s.args = [db]
+            end
+    
+            config.vm.provision "shell" do |s|
+                s.path = "./scripts/create-postgres.sh"
+                s.args = [db]
+            end
         end
     end
 
